0cb3c4c0ce7258982903a4dfad7b5d546301719c
ejagombar <ejagombar@gmail.com>
2021-11-09 12:01:39 +0000

    finished exercise 7

--- Worksheet5/Exercise2/mainwindow.cpp
+++ Worksheet5/Exercise2/mainwindow.cpp
@@ -1,45 +1,58 @@
+// mainwindow.cpp ------------------------------------------------------
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+//#include "stockitem.h"
 #include "edititemdialog.h"
 #include <QMessageBox>
+#include "stockitemlistmodel.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),ui(new Ui::MainWindow) {
+
+
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
-    // Your code here to connect button signals ...
+    // Link the ListModel to the ListView
+    ui->listView->setModel( &stockList );
+    // Tell this list view to only accept single selections
+    ui->listView->setSelectionBehavior( QAbstractItemView::SelectItems );
+    // Connect the released() signal of the addButton object to the handleAddButton slot in this object
 
     connect( ui->addButton, &QPushButton::released, this, &MainWindow::handleAddButton );
     connect( ui->editButton, &QPushButton::released, this, &MainWindow::handleEditButton );
     connect( ui->removeButton, &QPushButton::released, this, &MainWindow::handleRemoveButton );
-    // Connect the statusUpdateMessage() signal to the showMessage() slot of the status bar
     connect( this, &MainWindow::statusUpdateMessage, ui->statusBar, &QStatusBar::showMessage );
 }
 
-// Example of slot implementation in mainwindow.cpp---------------------
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
 void MainWindow::handleAddButton() {
-    // This causes MainWindow to emit the signal that will then be
-    // received by the statusbar's slot
+    StockItem item;
+    stockList.addItem( item );
     emit statusUpdateMessage( QString("Add button was clicked"), 0 );
+}
+void MainWindow::handleEditButton() {
+    EditItemDialog dialog( this );
+    QModelIndexList selectedList;
+    selectedList = ui->listView->selectionModel()->selectedIndexes();
+    if( selectedList.length() == 1 ) {
+        // selectedList is a list of all selected items in the listView. Since we set its
+        // behaviour to single selection, were only interested in the first selecteded item.
+        StockItem item = stockList.getItem( selectedList[0] );
+    if( dialog.runDialog( item ) ) {
+        // user clicked ok, need to update item in list...
+        stockList.setItem( item, selectedList[0] );
+    }
+    } else {
+        emit statusUpdateMessage( QString("No item selected to edit!"), 0 );
+    }
+}
+
+void MainWindow::handleRemoveButton() {
+    emit statusUpdateMessage( QString("Remove button was clicked"), 0 );
 }
 // ---------------------------------------------------------------------
 
-
-void MainWindow::handleRemoveButton() {
-    // This causes MainWindow to emit the signal that will then be
-    // received by the statusbar's slot
-    emit statusUpdateMessage( QString("Remove button was clicked"), 0 );
-
-
-}
-
-void MainWindow::handleEditButton() {
-    EditItemDialog dialog( this );
-    StockItem item;
-    emit statusUpdateMessage( QString("Edit button was clicked"), 0 );
-    dialog.runDialog( item );
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
 
--- Worksheet5/Exercise2/mainwindow.h
+++ Worksheet5/Exercise2/mainwindow.h
@@ -2,32 +2,34 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "stockitemlistmodel.h"
 
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
+
     Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        ~MainWindow();
 
-public slots:
+    public slots:
 
-    void handleAddButton();
-    void handleEditButton();
-    void handleRemoveButton();
+        void handleAddButton();
+        void handleEditButton();
+        void handleRemoveButton();
 
-signals:
-    void statusUpdateMessage( const QString & message, int timeout );
+    signals:
+        void statusUpdateMessage( const QString & message, int timeout );
 
 
-private:
-    Ui::MainWindow *ui;
+    private:
+        Ui::MainWindow *ui;
+        StockItemListModel stockList;
 };
 
 #endif // MAINWINDOW_H
