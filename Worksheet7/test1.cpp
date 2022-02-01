65c46bb8316d302acf6abd92007fd5a9127923de
ejagombar <ejagombar@gmail.com>
2022-02-01 09:46:44 +0000

    changed file in order to trigger travis

--- Worksheet7/test1.cpp
+++ Worksheet7/test1.cpp
@@ -3,8 +3,8 @@
 #include "adder.h"
 int main(int argc, char *argv[])
 {
-int a = 2;
-int b = 5;
-int c = add(a,b);
-return (c == 6) ? 0 : 1;
+	int a = 2;
+	int b = 5;
+	int c = add(a,b);
+	return (c == 6) ? 0 : 1;
 }
